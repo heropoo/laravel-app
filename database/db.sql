752ab51aa7ffa803e5863e19116c02185ebe71b3
heropoo <aiyouyou1000@163.com>
2018-10-14 15:55:57 +0800

    Update db.sql

--- database/db.sql
+++ database/db.sql
@@ -2,6 +2,7 @@ CREATE TABLE `admin` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '用户名',
   `password` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '密码',
+  `remember_token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '记住密码token',
   `status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0正常 -1禁止',
   `created_at` timestamp DEFAULT NULL,
   `updated_at` timestamp DEFAULT NULL,
